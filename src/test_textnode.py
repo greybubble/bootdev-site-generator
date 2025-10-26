db2a8d26ea8b19b7ddec480f2085599aa00f7933
greybubble <greybubble@gmail.com>
2025-10-26 16:43:00 -0500

    corrected testing

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -10,17 +10,17 @@ class TestTextNode(unittest.TestCase):
     def test_text_not_eq(self):
         node1 = TextNode("Here is some text", TextType.ITALIC)
         node2 = TextNode("Here is some text.", TextType.ITALIC)
-        self.assertEqual(node1, node2)
+        self.assertNotEqual(node1, node2)
 
     def test_text_node_not_eq(self):
         node1 = TextNode("Here is some text", TextType.ITALIC, "http://www.someaddress.com")
         node2 = TextNode("Here is some text", TextType.BOLD, "http://www.someaddress.com")
-        self.assertEqual(node1, node2)
+        self.assertNotEqual(node1, node2)
 
     def test_url_not_eq(self):
         node1 = TextNode("Here is some text", TextType.ITALIC, "http://www.someaddress.com")
         node2 = TextNode("Here is some text", TextType.ITALIC, "http://www.someotheraddress.com")
-        self.assertEqual(node1, node2)
+        self.assertNotEqual(node1, node2)
 
 
 if __name__ == "__main__":
